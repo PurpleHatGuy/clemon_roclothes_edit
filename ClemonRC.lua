1b4c3bd5453bc4e51649a331b05ccc16248a4d1e
orple <140215350+PurpleHatGuy@users.noreply.github.com>
2026-04-25 20:24:57 -0700

    67Update5.51 ClemonRC.lua

--- ClemonRC.lua
+++ ClemonRC.lua
@@ -44,8 +44,8 @@ function run(Player)
 	local SelectPlayer = Player.Name
 		--CONFIGURABLE PHYSICS--
 --rotation is boobs, position is ass--
-	local PositionPhysicsMultiply = 30.67
-	local RotationPhysicsMultiply = 3.67
+	local PositionPhysicsMultiply = 25.67
+	local RotationPhysicsMultiply = 3.867
 
 
 	function Function.PartListDefault()	
