3b973843294686b9e97f93bfab72873043f0c817
orple <140215350+PurpleHatGuy@users.noreply.github.com>
2026-04-25 23:54:25 -0700

    67Update5.53 ClemonRC.lua
    I fucked it up now its perfect

--- ClemonRC.lua
+++ ClemonRC.lua
@@ -2716,7 +2716,7 @@ function run(Player)
 			CurrentClothes = {},
 			PartList = Function.PartListDefault(),
 			BreastsScale = 2.367,
-			ButtsScale = 8.67,
+			ButtsScale = 09.67,
 			BodyPartPhysics = true,
 			CurrentPartList = {
 				Organ = {},
