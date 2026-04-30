335873590d04800f95fcfc902cf243ac9c071325
orple <140215350+PurpleHatGuy@users.noreply.github.com>
2026-04-30 01:50:02 -0700

    Update5.6 ClemonRC.lua

--- ClemonRC.lua
+++ ClemonRC.lua
@@ -2715,7 +2715,7 @@ function run(Player)
 --Edit values for DEFAULT size, saves you time to configure the scale--
 			CurrentClothes = {},
 			PartList = Function.PartListDefault(),
-			BreastsScale = 2,
+			BreastsScale = 2.3,
 			ButtsScale = 1.4,
 			BodyPartPhysics = true,
 			CurrentPartList = {
